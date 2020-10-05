475473e3bff12bfecb071b0b24c5f744b1941410
Pascal Menuet <>
2020-10-05 15:13:10 +0200

    web api

---
+++ testwebapi/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace testwebapi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
