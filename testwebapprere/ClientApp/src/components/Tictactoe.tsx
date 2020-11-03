4afea411f2613b8d875c29c9f4bd2e349efd9093
Menuet <menu-it@outlook.fr>
2020-11-03 23:31:05 +0100

    working draft

--- testwebapprere/ClientApp/src/components/Tictactoe.tsx
+++ testwebapprere/ClientApp/src/components/Tictactoe.tsx
@@ -10,12 +10,37 @@ type TictactoeProps =
     typeof TictactoeStore.actionCreators &
     RouteComponentProps<{}>;
 
+function hitText(hit: TictactoeStore.Hit | undefined): string {
+    switch (hit) {
+        case undefined:
+            return '';
+        case TictactoeStore.Hit.X:
+            return 'X';
+        case TictactoeStore.Hit.O:
+            return 'O';
+    }
+}
+
+function statusText(status: TictactoeStore.Status, nextIs: TictactoeStore.Hit): string {
+    switch (status) {
+        case TictactoeStore.Status.Pending: return "Next is " + (nextIs === TictactoeStore.Hit.X ? 'X' : 'O');
+        case TictactoeStore.Status.Draw: return "Draw";
+        case TictactoeStore.Status.Win: return "Winner is " + (nextIs === TictactoeStore.Hit.X ? 'O' : 'X');
+    }
+}
+
 class Tictactoe extends React.PureComponent<TictactoeProps> {
     public render() {
         const rows = this.props.grid.rows.map((row, rowIndex) => {
             const cells = row.cells.map((hit, cellIndex) => {
                 return (
-                    <button className="square" key={cellIndex}>{hit !== undefined ? (hit == TictactoeStore.Hit.X ? 'X' : 'O') : ''}</button>
+                    <button
+                        className="square"
+                        key={cellIndex}
+                        onClick={() => this.props.hit({ x: cellIndex, y: rowIndex })}
+                    >
+                        {hitText(hit)}
+                    </button>
                 );
             });
             return (
@@ -27,7 +52,7 @@ class Tictactoe extends React.PureComponent<TictactoeProps> {
                 <h1>TICTACTOE</h1>
                 <div className="game">
                     <div className="game-board">{rows}</div>
-                    <div className="game-info"></div>
+                    <div className="game-info">Status: {statusText(this.props.status, this.props.nextIs)}</div>
                 </div>
             </React.Fragment>
         );
--- testwebapprere/ClientApp/src/store/Tictactoe.ts
+++ testwebapprere/ClientApp/src/store/Tictactoe.ts
@@ -19,8 +19,16 @@ export type Location = {
     y: number;
 }
 
+export enum Status {
+    Pending,
+    Draw,
+    Win,
+}
+
 export interface TictactoeState {
     grid: Grid;
+    nextIs: Hit;
+    status: Status;
 }
 
 function createGrid(size: number): Grid {
@@ -31,14 +39,78 @@ function createGrid(size: number): Grid {
     return grid;
 }
 
-function hitGrid(grid: Grid, hit: Hit, location: Location): Grid {
-    const previous_hit = grid.rows[location.y].cells[location.x];
+function hitGrid(state: TictactoeState, location: Location): TictactoeState {
+    if (state.status !== Status.Pending)
+        return state;
+    const previous_hit = state.grid.rows[location.y].cells[location.x];
     if (previous_hit !== undefined)
-        return grid;
-    const new_grid: Grid = { rows: grid.rows, size: grid.size };
+        return state;
+    const new_grid: Grid = { rows: state.grid.rows, size: state.grid.size };
     new_grid.rows[location.y].cells = new_grid.rows[location.y].cells.slice();
-    new_grid.rows[location.y].cells[location.x] = hit;
-    return new_grid;
+    new_grid.rows[location.y].cells[location.x] = state.nextIs;
+    return {
+        grid: new_grid,
+        nextIs: state.nextIs === Hit.X ? Hit.O : Hit.X,
+        status: computeStatus(state.nextIs, new_grid, location),
+    };
+}
+
+function isRowWin(hit: Hit, grid: Grid, location: Location): boolean {
+    for (let x = 0; x < grid.size; ++x) {
+        if (grid.rows[location.y].cells[x] !== hit)
+            return false;
+    }
+    return true;
+}
+
+function isColWin(hit: Hit, grid: Grid, location: Location): boolean {
+    for (let y = 0; y < grid.size; ++y) {
+        if (grid.rows[y].cells[location.x] !== hit)
+            return false;
+    }
+    return true;
+}
+
+function isDiagDescending(hit: Hit, grid: Grid, location: Location): boolean {
+    if (location.x != location.y)
+        return false;
+    for (let i = 0; i < grid.size; ++i)
+        if (grid.rows[i].cells[i] !== hit)
+            return false;
+    return true;
+}
+
+function isDiagAscending(hit: Hit, grid: Grid, location: Location): boolean {
+    if (location.x != grid.size - 1 - location.y)
+        return false;
+    for (let i = 0; i < grid.size; ++i)
+        if (grid.rows[grid.size - 1 - i].cells[i] !== hit)
+            return false;
+    return true;
+}
+
+function isFull(grid: Grid): boolean {
+    for (let y = 0; y < grid.rows.length; ++y) {
+        for (let x = 0; x < grid.rows.length; ++x) {
+            if (grid.rows[y].cells[x] == undefined)
+                return false;
+        }
+    }
+    return true;
+}
+
+function computeStatus(hit: Hit, grid: Grid, location: Location): Status {
+    if (isRowWin(hit, grid, location))
+        return Status.Win;
+    if (isColWin(hit, grid, location))
+        return Status.Win;
+    if (isDiagDescending(hit, grid, location))
+        return Status.Win;
+    if (isDiagAscending(hit, grid, location))
+        return Status.Win;
+    if (isFull(grid))
+        return Status.Draw;
+    return Status.Pending;
 }
 
 // -----------------
@@ -46,7 +118,7 @@ function hitGrid(grid: Grid, hit: Hit, location: Location): Grid {
 // They do not themselves have any side-effects; they just describe something that is going to happen.
 // Use @typeName and isActionType for type detection that works even after serialization/deserialization.
 
-export interface HitAction { type: 'HIT', hit : Hit, location : Location }
+export interface HitAction { type: 'HIT', location : Location }
 
 // Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
 // declared type strings (and not any other arbitrary string).
@@ -57,7 +129,7 @@ export type KnownAction = HitAction;
 // They don't directly mutate state, but they can have external side-effects (such as loading data).
 
 export const actionCreators = {
-    hit: (hit: Hit, location: Location) => ({ type: 'HIT', hit : hit, location : location } as HitAction)
+    hit: (location: Location) => ({ type: 'HIT', location : location } as HitAction)
 };
 
 // ----------------
@@ -65,13 +137,13 @@ export const actionCreators = {
 
 export const reducer: Reducer<TictactoeState> = (state: TictactoeState | undefined, incomingAction: Action): TictactoeState => {
     if (state === undefined) {
-        return { grid: createGrid(2) };
+        return { grid: createGrid(3), nextIs: Hit.X, status: Status.Pending };
     }
 
     const action = incomingAction as KnownAction;
     switch (action.type) {
         case 'HIT':
-            return { grid: hitGrid(state.grid, action.hit, action.location) };
+            return hitGrid(state, action.location);
         default:
             return state;
     }
