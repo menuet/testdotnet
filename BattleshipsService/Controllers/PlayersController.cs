5ae7c03d8c1eb7b20826c3fa07dd8a03c5850a1f
Menuet <menu-it@outlook.fr>
2021-05-28 09:39:40 +0200

    unfinished

---
+++ BattleshipsService/Controllers/PlayersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleshipsService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayersController : ControllerBase
+    {
+        private static readonly string[] PlayersNames = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PlayersController> _logger;
+
+        public PlayersController(ILogger<PlayersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Player> Get()
+        {
+            return PlayersNames.Select(pn => new Player { Name = pn });
+        }
+    }
+}
---
+++ BattleshipsService/Player.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace BattleshipsService
+{
+    public class Player
+    {
+        public string Name { get; set; }
+    }
+}
